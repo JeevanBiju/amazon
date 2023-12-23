e08af292c55650bdee56f487e484a72b21be6894
Jeevan Biju <107880615+JeevanBiju@users.noreply.github.com>
2023-12-23 23:07:05 +0530

    Create ProductAdapter.kt

---
+++ ProductAdapter.kt
@@ -0,0 +1,8 @@
+class ProductAdapter(private val products: List<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+
+    // Implement RecyclerView Adapter methods
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // ViewHolder implementation
+    }
+}
